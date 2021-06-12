a60e1d79fab3ee4fa3258a10611498c18b6a3d52
Lev Valuev <lev.valuev@gmail.com>
2021-06-12 18:48:23 +0300

    It doesn't have tests and documentation, it has not so handsome output, but it works!!

--- parser.go
+++ parser.go
@@ -6,46 +6,59 @@ import (
 	"io"
 )
 
-var links map[address]*Link
+var links []*Link
 
-func ParseLinks(r io.Reader) (l map[address]*Link) {
-	l = make(map[address]*Link)
+func ParseLinks(r io.Reader) []*Link {
+	l := &links
 	node, err := html.Parse(r)
 	if err != nil {
 		fmt.Println(err)
 	}
 	crawl(node, l)
-	return l
+	return links
 }
 
-func crawl(n *html.Node, l map[address]*Link) {
+func crawl(n *html.Node, l *[]*Link) {
 	checkForATypes(n, l)
 	if n.NextSibling != nil {
 		crawl(n.NextSibling, l)
 	}
 	if n.FirstChild != nil {
-		crawl(n.FirstChild, l	)
+		crawl(n.FirstChild, l)
 	}
 }
 
-func checkForATypes(n *html.Node, l map[address]*Link) {
+func checkForATypes(n *html.Node, l *[]*Link) {
 	if n.DataAtom == 1 {
 		for _, index := range n.Attr {
 			if index.Key == "href" {
 				var link Link
 				link.Href = index.Val
-				link.Text = aCrawler(n.FirstChild)
-				l[n] = &link
+				link.Text = *aCrawler(n.FirstChild, &link.Text)
+				*l = append(*l, &link)
 			}
 		}
 	}
 }
 
-func aCrawler(n *html.Node) (text string) {
+func aCrawler(n *html.Node, text *string)  *string {
+	var tmpText string
 	if n.Type == html.TextNode {
-		text = n.Data
+		*text = *text + " " + n.Data
 	}
-	return
+	if n.NextSibling != nil {
+		tmpText = *aCrawler(n.NextSibling, &tmpText)
+		if tmpText != "" {
+			*text = *text + " " + tmpText
+		}
+	}
+	if n.FirstChild != nil {
+		tmpText = *aCrawler(n.FirstChild, &tmpText)
+		if tmpText != "" {
+			 *text = *text + " " + tmpText
+		}
+	}
+	return text
 }
 
 type address *html.Node
